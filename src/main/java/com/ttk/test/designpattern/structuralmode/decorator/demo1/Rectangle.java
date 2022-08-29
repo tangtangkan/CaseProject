80f7b7572901a0503a87f7af15a6251ffccfa62d
tangtangkan <1344423183@qq.com>
2022-08-29 23:57:22 +0800

    装饰器模式

---
+++ src/main/java/com/ttk/test/designpattern/structuralmode/decorator/demo1/Rectangle.java
@@ -0,0 +1,12 @@
+package com.ttk.test.designpattern.structuralmode.decorator.demo1;
+
+/**
+ * 长方形
+ */
+public class Rectangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("画长方形");
+    }
+}
---
+++ src/main/java/com/ttk/test/designpattern/structuralmode/decorator/demo1/Shape.java
@@ -0,0 +1,10 @@
+package com.ttk.test.designpattern.structuralmode.decorator.demo1;
+
+/**
+ * 形状
+ */
+public interface Shape {
+
+    void draw();
+
+}
