17eb84545cc10d22cabea3d1fc87f876be23f025
tangtangkan <tangtangkan@jingkunsystem.com>
2022-08-29 19:10:19 +0800

    组合模式

---
+++ src/main/java/com/ttk/test/designpattern/structuralmode/combination/demo1/Employee.java
@@ -0,0 +1,41 @@
+package com.ttk.test.designpattern.structuralmode.combination.demo1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 员工类
+ */
+public class Employee {
+
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    //构造函数
+    public Employee(String name, String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public String toString() {
+        return ("Employee :[ Name : " + name
+                + ", dept : " + dept + ", salary :"
+                + salary + " ]");
+    }
+}
